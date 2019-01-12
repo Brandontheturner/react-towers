9b35a4b8e9fc12e0294bef6675a2618d72280af5
Brandon Turner <brandontheturner@gmail.com>
2019-01-12 17:46:06 -0600

    working on towers move logic

--- src/script.js
+++ src/script.js
@@ -4,19 +4,78 @@ import "./style.css";
 class TowersOfHanoi extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      a: [4, 3, 1],
+      b: [2],
+      c: [],
+      gameWon: false
+    };
   }
+  onDragOver = event => {
+    event.preventDefault();
+  };
 
+  onDragStart = (event, disc, startStack) => {
+    console.log("dragStart", disc, startStack);
+    event.dataTransfer.setData("disc", disc);
+    event.dataTransfer.setData("startStack", startStack);
+  };
+
+  onDrop = (event, endStack) => {
+    const disc = event.dataTransfer.getData("disc");
+    const startStack = event.dataTransfer.getData("startStack");
+  };
   render() {
     return (
       <div>
-        <div data-stack="1">
-          <div data-block="100" />
-          <div data-block="75" />
-          <div data-block="50" />
-          <div data-block="25" />
+        <div
+          data-stack="a"
+          onDragOver={this.onDragOver}
+          onDrop={e => this.onDrop(e, "a")}
+        >
+          {this.state.a.map(num => {
+            return (
+              <div
+                onDragStart={e => this.onDragStart(e, num, "a")}
+                draggable
+                key={num}
+                data-block={num * 25}
+              />
+            );
+          })}
         </div>
-        <div data-stack="2" />
-        <div data-stack="3" />
+        <div
+          data-stack="b"
+          onDragOver={this.onDragOver}
+          onDrop={e => this.onDrop(e, "b")}
+        >
+          {this.state.b.map(num => {
+            return (
+              <div
+                onDragStart={e => this.onDragStart(e, num, "b")}
+                draggable
+                key={num}
+                data-block={num * 25}
+              />
+            );
+          })}
+        </div>
+        <div
+          data-stack="c"
+          onDragOver={this.onDragOver}
+          onDrop={e => this.onDrop(e, "c")}
+        >
+          {this.state.c.map(num => {
+            return (
+              <div
+                onDragStart={e => this.onDragStart(e, num, "c")}
+                draggable
+                key={num}
+                data-block={num * 25}
+              />
+            );
+          })}
+        </div>
       </div>
     );
   }
